55d1557dda12906c6423aa70f04c94abfc29672b
thinhhnt <thinh.researcher@gmail.com>
2023-03-14 09:49:17 +0700

    Update

--- test_hf.py
+++ test_hf.py
@@ -1,7 +1,10 @@
+from typing import Dict
+
 import numpy as np
 from datasets import load_dataset
 from matplotlib import cm
 from PIL import Image
+from torch import Tensor
 from transformers import AutoImageProcessor, AutoModel
 
 model = AutoModel.from_pretrained("RGBD-SOD/bbsnet", trust_remote_code=True)
@@ -24,23 +27,37 @@ sample = {
 """
 sample = dataset[index]
 
+depth: Image.Image = sample["depth"]
+rgb: Image.Image = sample["rgb"]
+gt: Image.Image = sample["gt"]
+name: str = sample["name"]
+
 
 """
+1. Preprocessing step
+
 preprocessed_sample = {
     'rgb': tensor([[[[-0.8507, ....0365]]]]), 
     'gt': tensor([[[[0., 0., 0...., 0.]]]]), 
     'depth': tensor([[[[0.9529, 0....3490]]]])
 }
 """
-preprocessed_sample = image_processor.preprocess(sample)
+preprocessed_sample: Dict[str, Tensor] = image_processor.preprocess(sample)
 
 """
+2. Prediction step
+
 output = {
     'logits': tensor([[[[-5.1966, ...ackward0>)
 }
 """
-output = model(preprocessed_sample["rgb"], preprocessed_sample["depth"])
+output: Dict[str, Tensor] = model(
+    preprocessed_sample["rgb"], preprocessed_sample["depth"]
+)
 
+"""
+3. Postprocessing step
+"""
 postprocessed_sample: np.ndarray = image_processor.postprocess(
     output["logits"], [sample["gt"].size[1], sample["gt"].size[0]]
 )
@@ -50,4 +67,4 @@ prediction = Image.fromarray(np.uint8(cm.gist_earth(postprocessed_sample) * 255)
 Show the predicted salient map and the corresponding ground-truth(GT)
 """
 prediction.show()
-sample["gt"].show()
+gt.show()
